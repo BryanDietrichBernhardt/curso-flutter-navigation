a3b68b43d775d2c9138d92860468808b28b8b359
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-03-10 03:08:53 -0300

    Route params

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,14 @@ void main() {
   runApp(MyApp());
 }
 
+// classe para passar argumentos por rotas
+class SecondScreenArgs {
+  final int id;
+  final String name;
+
+  SecondScreenArgs(this.id, this.name);
+}
+
 // precisa obrigatóriamente ter um StatelessWidget inicialmente para chamar o home e inicial as rotas das telas
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,7 +23,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => FirstScreen(),
-        '/secondScreen': (context) => SecondScreen(),
+        SecondScreen.routeName: (context) => SecondScreen()
+        //'/secondScreen': (context) => SecondScreen(),
       },
     );
   }
@@ -26,6 +35,8 @@ class FirstScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int count = 1;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -35,7 +46,9 @@ class FirstScreen extends StatelessWidget {
           body: Center(
             child: ElevatedButton(
               onPressed: () {
-                Navigator.pushNamed(context, "/secondScreen");
+                Navigator.pushNamed(context, "/secondScreen",
+                    arguments: SecondScreenArgs(1, "Bryan $count"));
+                count++;
                 // Navigator.push(context, MaterialPageRoute(builder: (context) {
                 //   return const SecondScreen();
                 // }));
@@ -48,15 +61,21 @@ class FirstScreen extends StatelessWidget {
 }
 
 class SecondScreen extends StatelessWidget {
+  // identificação da rota
+  static const routeName = "/secondScreen";
+
   const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final arguments =
+        ModalRoute.of(context)!.settings.arguments as SecondScreenArgs;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Second Page"),
+          title: Text("Second Page ${arguments.name}"),
           backgroundColor: Colors.red,
         ),
         body: Center(
